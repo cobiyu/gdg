9c2c0f3297a7bb166c7f5b60b192176c455a32ae
aliwo <aliwo@naver.com>
2019-05-18 14:00:37 +0900

    giphy axios service 추가

---
+++ src/helper/GiphyService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const APIKEY = 'cWQytn3bMoHqQlqAmLNe7YwJCapZLMXd';
+
+const service = axios.create({
+  baseURL: 'http://api.giphy.com/v1',
+  timeout: 5000,
+});
+
+export const searchGIFs = (query, limit) => {
+  service({
+      url: '/gifs/search?' + 'q=' + query + '&limit=' + limit + '&api_key=' + APIKEY
+  })
+};
