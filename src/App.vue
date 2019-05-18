0721da93c8d1d98fb473e3b8d1a73dd9d80f14e2
cobiyu <yhh@gabia.com>
2019-05-18 13:51:04 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-col :span="24"><div class="grid-content bg-purple-dark">header</div></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <el-row>
+          <el-col :span="24">
+            <div class="grid-content bg-purple-dark">
+              <div style="text-align: left;">
+                user
+              </div>
+            </div>
+            <div class="grid-content bg-purple-dark">
+              <div style="text-align: right;">
+                my
+              </div>
+            </div>
+            <div class="grid-content bg-purple-dark">
+              <div style="text-align: right;">
+                my
+              </div>
+            </div>
+            <div class="grid-content bg-purple-dark">
+              <div style="text-align: right;">
+                my
+              </div>
+            </div>
+            <div class="grid-content bg-purple-dark">
+              <div style="text-align: right;">
+                my
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+      </el-col>
+
+      <el-col :span="12">
+        <el-row>
+          <el-col :span="24">
+            <div class="grid-content bg-purple-dark">search</div>
+          </el-col>
+          <el-col :span="24">
+            <div class="grid-content bg-purple-dark">input</div>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .el-row {
+    margin-bottom: 20px;
+
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
